8dca92adf260d2c39ab8d3d3c5da418cb57f88fd
Adam McMaster <me@adam-mcmaster.com>
2025-07-23 15:38:27 +0100

    Fix module name

--- start_server.sh
+++ start_server.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [ "$DJANGO_ENV" == "production" ]; then
-  export DJANGO_SETTINGS_MODULE="sltom.settings_production"
+  export DJANGO_SETTINGS_MODULE="sl_tom.settings_production"
   
   echo Applying migrations
   python manage.py migrate --noinput
-  if [ ! -d "sltom/static" ]; then
+  if [ ! -d "sl_tom/static" ]; then
     echo Generating static files
     python manage.py collectstatic --clear --no-input
   fi
   echo Starting production server
-  exec gunicorn sltom.wsgi -b 0:8080 --access-logfile - --capture-output
+  exec gunicorn sl_tom.wsgi -b 0:8080 --access-logfile - --capture-output
 else
   echo Applying migrations
   python manage.py migrate --noinput
