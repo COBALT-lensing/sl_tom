a0617c53802098acefc12b2886bd7f08b4c3590e
Adam McMaster <me@adam-mcmaster.com>
2025-07-25 16:58:36 +0100

    Add sentry-debug route

--- sl_tom/urls.py
+++ sl_tom/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.urls import path, include
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path('', include('tom_common.urls')),
+    path('sentry-debug/', trigger_error),
 ]
