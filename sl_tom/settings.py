cdec28a86883f07aadab70049de0fabe923f7844
Adam McMaster <me@adam-mcmaster.com>
2025-07-29 12:35:31 +0100

    Move sentry to main settings.py

--- sl_tom/settings.py
+++ sl_tom/settings.py
@@ -348,7 +348,24 @@ ZOONIVERSE_COMMIT_CHANGES = False
 ZOONIVERSE_PROJECT_ID = 14770
 ZOONIVERSE_WORKFLOW_ID = 25070
 
+if "SENTRY_DSN" in os.environ:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=os.environ["SENTRY_DSN"],
+        integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
+
+
 try:
-    from local_settings import *  # noqa
+    from sl_tom.local_settings import *  # noqa
 except ImportError:
     pass
