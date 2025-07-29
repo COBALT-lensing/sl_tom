db445bd91ab390462ed480fffc3ea059e1610220
Adam McMaster <me@adam-mcmaster.com>
2025-07-29 14:29:38 +0100

    Require login

--- zooniverse/views.py
+++ zooniverse/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from zooniverse.models import (
     ZooniverseSubject,
     ZooniverseClassification,
@@ -9,10 +11,10 @@ from zooniverse.models import (
 )
 
 
-class ZooniverseTargetDetailView(DetailView):
+class ZooniverseTargetDetailView(LoginRequiredMixin, DetailView):
     model = ZooniverseTarget
 
 
-class ZooniverseTargetListView(ListView):
+class ZooniverseTargetListView(LoginRequiredMixin, ListView):
     model = ZooniverseTarget
     paginate_by = 100
