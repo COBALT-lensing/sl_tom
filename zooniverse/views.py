a5491887552abe55a9e3da17f1cdd3faac530701
Adam McMaster <me@adam-mcmaster.com>
2025-08-06 15:18:16 +0100

    Remove unused imports

--- zooniverse/views.py
+++ zooniverse/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from zooniverse.models import (
-    ZooniverseSubject,
-    ZooniverseClassification,
     ZooniverseTarget,
 )
 
